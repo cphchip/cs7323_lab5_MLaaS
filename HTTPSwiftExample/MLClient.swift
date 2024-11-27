97dea93bee240fd2b072eb059be2dd79cf11dd38
chessmith <chessmith@live.com>
2024-11-26 23:16:47 -0600

    comments

--- HTTPSwiftExample/MLClient.swift
+++ HTTPSwiftExample/MLClient.swift
@@ -17,17 +17,24 @@ protocol MLClientProtocol {
 }
 
 class MLClient {
-
+    // API endpoint
     private let API_BASE_ENDPOINT = "http://45.33.24.52:8000"
 
+    // secret API token
     private let API_TOKEN =
         Bundle.main.object(forInfoDictionaryKey: "API_TOKEN") as? String ?? ""
+    
     public var delegate: MLClientProtocol?
+    
+    // are we currently fetching labels?
     private var fetchingLabels = false
+    
+    // local copy of labels
     private var labels: [Dataset] = []
 
-    // Fetch all labels
+    // get all labels
     func getLabels() -> [Dataset] {
+        // Wait for the labels to be fetched if they are currently being fetched
         if fetchingLabels {
             var count = 0
             while fetchingLabels && count < 10 {
@@ -132,27 +139,33 @@ class MLClient {
         return labels.first { $0.label == name }
     }
 
+    // Fetch all labels from the server
     func fetchLabels() async throws -> [Dataset] {
         if fetchingLabels {
+            // If a labels request is already in progress, return an error
             throw APIError.labelsRequestInProgress
         }
         fetchingLabels = true
-        defer { fetchingLabels = false }
+        defer { fetchingLabels = false } // reset the flag when done
 
         guard let url = URL(string: "\(API_BASE_ENDPOINT)/labels") else {
             throw APIError.invalidURL
         }
-
+    
+        // Create the request
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.addValue(API_TOKEN, forHTTPHeaderField: "x-api-token")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Send the request
         do {
             let (data, response) = try await URLSession.shared.data(
                 for: request)
             if let httpResponse = response as? HTTPURLResponse,
                 httpResponse.statusCode == 200
             {
+                // successful response
                 print("Response: \(String(data: data, encoding: .utf8) ?? "")")
                 let labels = try JSONDecoder().decode(
                     [Dataset].self, from: data)
@@ -167,6 +180,7 @@ class MLClient {
     }
 
     init() {
+        // Fetch labels on initialization
         Task {
             do {
                 self.labels = try await fetchLabels()
@@ -179,12 +193,17 @@ class MLClient {
         }
     }
 
+    
     func uploadImage(image: UIImage, dsid: Int) {
+        /// Upload an image to the server
         Task {
+            // start the upload
             await callUploadImage(image: image, dsid: dsid) { result in
+                // handle the result
                 switch result {
                 case .success(let message):
                     print("Image upload complete: \(message)")
+                    // notify the delegate
                     DispatchQueue.main.async {
                         self.delegate?.uploadImageComplete(
                             success: true, errMsg: nil
@@ -193,6 +212,7 @@ class MLClient {
                 case .failure(let error):
                     print(
                         "Error uploading image: \(error.localizedDescription)")
+                    // notify the delegate
                     DispatchQueue.main.async {
                         self.delegate?.uploadImageComplete(
                             success: false, errMsg: error.localizedDescription
@@ -207,12 +227,14 @@ class MLClient {
         image: UIImage, dsid: Int,
         completion: @escaping (Result<String, APIError>) -> Void
     ) async {
+        // Validate the server URL
         guard let serverURL = URL(string: "\(API_BASE_ENDPOINT)/upload_image")
         else {
             completion(.failure(.invalidURL))
             return
         }
-
+        
+        // Compress the image
         guard let imageData = image.jpegData(compressionQuality: 0.8) else {
             completion(.failure(.decodingError("Unable to compress image")))
             return
@@ -222,10 +244,10 @@ class MLClient {
         var multipart = MultipartRequest()
         multipart.add(key: "dsid", value: String(dsid))  // Add ID
         multipart.add(
-            key: "image",  // Key for the image field
-            fileName: "image.jpg",  // Image file name
-            fileMimeType: "image/jpeg",  // Image MIME type
-            fileData: imageData  // Image binary data
+            key: "image",
+            fileName: "image.jpg",
+            fileMimeType: "image/jpeg",
+            fileData: imageData
         )
 
         // Create the HTTP request
@@ -247,36 +269,46 @@ class MLClient {
             if let httpResponse = response as? HTTPURLResponse,
                 (200...299).contains(httpResponse.statusCode)
             {
+                // handle a successful response
                 let json = convertDataToDictionary(with: data)
                 let message = json["message"] as? String ?? "Upload successful"
                 completion(.success(message))
                 return
             }
+            // bad response
             completion(.failure(.noData))
             return
         } catch {
+            // really bad
             completion(.failure(.networkError(error)))
             return
         }
     }
 
     func predict(image: UIImage, dsid: Int, model_t: String) {
+        /// request a prediction from the server
         Task {
+            // make a request to the server
             await callPredict(image: image, dsid: dsid, model_t: model_t) {
                 result in
+                // handle the result
                 switch result {
                 case .success(let tid):
                     print("predicting, task id: \(tid)")
+                    // after 3 seconds, start checking the task status
                     DispatchQueue.global().asyncAfter(deadline: .now() + 3) {
                         self.waitFor(taskId: tid) { result in
                             switch result {
+                                // check the task result
                             case .success(let value):
                                 print("prediction complete: \(value)")
+                                // notify the delegate
                                 DispatchQueue.main.async {
                                     self.delegate?.predictionComplete(
                                         result: value, error: nil)
                                 }
                             case .failure(let error):
+                                // task failed
                                 print(
                                     "Error Making prediction: \(error.localizedDescription)"
                                 )
@@ -288,6 +320,7 @@ class MLClient {
                         }
                     }
                 case .failure(let error):
+                    // sending the request failed
                     print(
                         "Error requesting prediction: \(error.localizedDescription)"
                     )
@@ -295,16 +328,20 @@ class MLClient {
             }
         }
     }
+    
+    // make a prediction request to the server
     private func callPredict(
         image: UIImage, dsid: Int, model_t: String,
         completion: @escaping (Result<String, APIError>) -> Void
     ) async {
         guard let serverURL = URL(string: "\(API_BASE_ENDPOINT)/predict")
         else {
+            // invalid URL
             completion(.failure(.invalidURL))
             return
         }
 
+        // compress the image
         guard let imageData = image.jpegData(compressionQuality: 0.8) else {
             completion(.failure(.decodingError("Unable to compress image")))
             return
@@ -315,10 +352,10 @@ class MLClient {
         multipart.add(key: "dsid", value: String(dsid))
         multipart.add(key: "model_t", value: model_t)
         multipart.add(
-            key: "image",  // Key for the image field
-            fileName: "image.jpg",  // Image file name
-            fileMimeType: "image/jpeg",  // Image MIME type
-            fileData: imageData  // Image binary data
+            key: "image",
+            fileName: "image.jpg",
+            fileMimeType: "image/jpeg",
+            fileData: imageData
         )
 
         // Create the HTTP request
@@ -355,29 +392,39 @@ class MLClient {
         completion: @escaping (Result<[String: Any], APIError>) -> Void
     ) {
         print("Waiting for task \(tid) to complete")
+        
+        // only wait for a limited number of retries
         if retries <= 0 {
             completion(.failure(.maximumRetriesExceeded))
             return
         }
 
+        // validate the URL
         guard let url = URL(string: "\(API_BASE_ENDPOINT)/tasks/\(tid)") else {
             completion(.failure(.invalidURL))
             return
         }
+        
+        // create the request
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.addValue(API_TOKEN, forHTTPHeaderField: "x-api-token")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // send the request
         let task = URLSession.shared.dataTask(with: request) {
             data, response, error in
             if let error = error {
+                // something went wrong sending the request
                 completion(.failure(.networkError(error)))
                 return
             }
-
+            
+            // make sure the server responded with a success status code
             guard let httpResponse = response as? HTTPURLResponse,
                 httpResponse.statusCode == 200
             else {
+                // bad server response, try to get the details
                 var detail = ""
                 if let data = data {
                     let json = self.convertDataToDictionary(with: data)
@@ -392,9 +439,10 @@ class MLClient {
                 )
                 return
             }
-
+            // success, parse the response
             if let data = data {
                 do {
+                    // decode the JSON
                     let json =
                         try JSONSerialization.jsonObject(
                             with: data,
@@ -402,12 +450,14 @@ class MLClient {
                     if let json = json {
                         let status = json["status"] as? String ?? ""
                         if status == "Success" {
+                            // task completed successfully
                             print("Task completed: \(json)")
                             let result =
                                 json["result"] as? [String: Any] ?? [:]
                             completion(.success(result))
                             return
                         } else if status == "Failed" {
+                            // task failed
                             completion(
                                 .failure(
                                     .taskFailed(
@@ -415,10 +465,12 @@ class MLClient {
                             )
                             return
                         } else {
-                            // Task is still pending, wait and try again
+                            // Task is still pending, wait and try again after 3 seconds
                             DispatchQueue.global().asyncAfter(
                                 deadline: .now() + 3
                             ) {
+                                // recursive call to wait for the task to complete
+                                // with one less retry
                                 self.waitFor(
                                     taskId: tid, retries: retries - 1,
                                     completion: completion)
@@ -426,6 +478,7 @@ class MLClient {
                             return
                         }
                     } else {
+                        // failed to parse the JSON
                         print("Error: Unable to parse JSON")
                         print(
                             "Response: \(String(data: data, encoding: .utf8) ?? "No response data")"
@@ -435,6 +488,7 @@ class MLClient {
                         return
                     }
                 } catch {
+                    // something went wrong
                     print("Error: \(error.localizedDescription)")
                     completion(
                         .failure(.decodingError(error.localizedDescription)))
@@ -445,22 +499,29 @@ class MLClient {
         task.resume()
     }
 
+    
     func trainModel(dsid: Int, model_t: String) {
+    /// Train a model on the server
         Task {
+            // start the training
             callTrainModel(dsid: dsid, model_t: model_t) { result in
+                // handle the response
                 switch result {
                 case .success(let tid):
                     print("Model training, task id: \(tid)")
+                    // the training request was accepted, wait 3 seconds then start checking the task status
                     DispatchQueue.global().asyncAfter(deadline: .now() + 3) {
                         self.waitFor(taskId: tid) { result in
                             switch result {
                             case .success(let value):
+                                // task completed successfully
                                 print("Model training complete: \(value)")
                                 DispatchQueue.main.async {
                                     self.delegate?.modelTrainingComplete(
                                         result: value, error: nil)
                                 }
                             case .failure(let error):
+                                // task failed
                                 print(
                                     "Error training model: \(error.localizedDescription)"
                                 )
@@ -484,13 +545,16 @@ class MLClient {
         dsid: Int, model_t: String,
         completion: @escaping (Result<String, APIError>) -> Void
     ) {
+        // Validate the server URL
         guard let url = URL(string: "\(API_BASE_ENDPOINT)/train_model") else {
             print("Invalid URL")
             return
         }
-
+        
+        // train request to be added to the request body
         let trainReq = TrainRequest(dsid: dsid, model_t: model_t)
 
+        // Create the request
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -502,16 +566,20 @@ class MLClient {
             return
         }
 
+        // Send the request
         let task = URLSession.shared.dataTask(with: request) {
             data, response, error in
             if let error = error {
+                // request not sent
                 completion(.failure(.networkError(error)))
                 return
             }
 
+            // check the server response
             guard let httpResponse = response as? HTTPURLResponse,
                 httpResponse.statusCode == 202
             else {
+                // not a successful response
                 var detail = ""
                 if let data = data {
                     let json = self.convertDataToDictionary(with: data)
@@ -525,9 +593,11 @@ class MLClient {
                 )
                 return
             }
-
+            
+            // http response is good, check the value of the response
             if let data = data {
                 do {
+                    // parse the JSON response
                     let json =
                         try JSONSerialization.jsonObject(
                             with: data,
@@ -535,7 +605,7 @@ class MLClient {
                     if let json = json {
                         let tid = json["task_id"] as? String ?? ""
                         completion(.success(tid))
-
+                        return
                     } else {
                         print("Error: Unable to parse JSON")
                         print(
@@ -543,11 +613,13 @@ class MLClient {
                         )
                         completion(
                             .failure(.decodingError("Unable to parse JSON")))
+                        return
                     }
                 } catch {
                     print("Error: \(error.localizedDescription)")
                     completion(
                         .failure(.decodingError(error.localizedDescription)))
+                    return
                 }
             }
         }
